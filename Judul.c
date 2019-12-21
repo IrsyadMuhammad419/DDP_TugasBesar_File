31fe0131f49cc6f48cdefc0058615c2dbec06957
Icad <54462897+IrsyadMuhammad419@users.noreply.github.com>
2019-12-21 14:20:46 +0700

    Add files via upload

---
+++ Judul.c
@@ -0,0 +1,92 @@
+/* -----------------------------------------------
+Program 		:	Judul.c
+Deskripsi		: 	Modul Tampilan judul permainan (CCC) + fullscreen
+Author 			: 	Irsyad Muhammad (191524014)
+Versi/tanggal	: 	21/12/2019 
+Compiler 		:	TDM-GCC 4.9.6.2 64-bit release
+---------------------------------------------------*/
+#include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+
+void fullscreen(){
+	keybd_event(VK_MENU,0x38,0,0);
+	keybd_event(VK_RETURN,0x1c,0,0);
+	keybd_event(VK_RETURN,0x1c,KEYEVENTF_KEYUP,0);
+	keybd_event(VK_MENU,0x38,KEYEVENTF_KEYUP,0);
+}
+
+void gotoxy(int x, int y);
+void gotoxy(int x, int y){
+//modul untuk memfungsikan fungsi gotoxy	
+	HANDLE hConsoleOutput;  
+	COORD dwCursorPosition;  
+	dwCursorPosition.X = x;  
+	dwCursorPosition.Y = y;  
+	hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);  
+	SetConsoleCursorPosition(hConsoleOutput,dwCursorPosition);   
+}
+
+void judul(){
+// Author	: Irsyad Muhammad
+
+// Procedur untuk menampilkan judul permainan
+// I.S		: Layar menampilkan blankpage
+// F.S		: Layar menampilkan judul permainan "CCC :Circle Cross Challange"
+
+/*BEGIN PROCEDURE_JUDUL*/	
+	/*TAMPILAN CCC*/
+	
+	/*baris 1*/
+	gotoxy(58,5); printf("лллллллллллл\n");
+	gotoxy(76,5); printf("лллллллллллл\n");
+	gotoxy(94,5); printf("лллллллллллл\n");
+	
+	/*baris 2*/
+	gotoxy(58,6); printf("лллллллллллл\n");
+	gotoxy(76,6); printf("лллллллллллл\n");
+	gotoxy(94,6); printf("лллллллллллл\n");
+	
+	/*baris 3*/
+	gotoxy(58,7); printf("ллллл\n");
+	gotoxy(76,7); printf("ллллл\n");
+	gotoxy(94,7); printf("ллллл\n");
+	
+	/*baris 4*/
+	gotoxy(58,8); printf("ллллл\n");
+	gotoxy(76,8); printf("ллллл\n");
+	gotoxy(94,8); printf("ллллл\n");
+	
+	/*baris 5*/
+	gotoxy(58,9); printf("ллллл\n");
+	gotoxy(76,9); printf("ллллл\n");
+	gotoxy(94,9); printf("ллллл\n");
+	
+	/*baris 6*/
+	gotoxy(58,10); printf("ллллл\n");
+	gotoxy(76,10); printf("ллллл\n");
+	gotoxy(94,10); printf("ллллл\n");
+		
+	/*baris 7*/
+	gotoxy(58,11); printf("лллллллллллл\n");
+	gotoxy(76,11); printf("лллллллллллл\n");
+	gotoxy(94,11); printf("лллллллллллл\n");
+	
+	/*baris 8*/
+	gotoxy(58,12); printf("лллллллллллл\n");
+	gotoxy(76,12); printf("лллллллллллл\n");
+	gotoxy(94,12); printf("лллллллллллл\n");
+	/*END TAMPILAN CCC*/
+	
+	/*TAMPILAN CIRCLE CROSS CHALLANGE*/
+	gotoxy(70,14);printf("CIRCLE CROSS CHALLENGES\n\n");
+	/*END TAMPILAN CIRCLE CROSS CHALLANGE*/
+	
+/*END PROCEDURE_JUDUL*/
+}
+
+
+int main(){
+	fullscreen();
+	judul();
+}
